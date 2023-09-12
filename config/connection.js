f3dd96d9daf9bbf327fc5721737e2afff9f098b9
jenzinha <jamili.amaral@estudante.ifms.edu.br>
2023-09-12 17:05:17 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,13 @@ const { Sequelize } = require('sequelize');
 const config = require('../config/config')
 require('dotenv').config();
 
-const sequelize = new Sequelize(    );
+const sequelize = new Sequelize( config );
 
 try {
   sequelize.authenticate();
-  console.log('              ');
+  console.log(' conectado com banco de dados ');
 } catch (error) {
-  console.error('             ', error);
+  console.error(' conexão falhou  ', error);
 }
 
 module.exports = { Sequelize, sequelize };
